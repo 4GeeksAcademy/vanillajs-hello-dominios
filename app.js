3a41542d2d4612369a8ec50a3a9a1ffa542d1c7c
AndreuFerrandis <153548161+AndreuFerrandis@users.noreply.github.com>
2024-02-14 19:53:28 +0000

    proyecto acabado

---
+++ app.js
@@ -0,0 +1,19 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+function generateDomainNames(pronouns, adjectives, nouns) {
+    let domainNames = [];
+    for (let i = 0; i < pronouns.length; i++) {
+        for (let j = 0; j < adjectives.length; j++) {
+            for (let k = 0; k < nouns.length; k++) {
+                let domainName = pronouns[i] + adjectives[j] + nouns[k] + ".com";
+                domainNames.push(domainName);
+            }
+        }
+    }
+    return domainNames;
+}
+
+let domainNames = generateDomainNames(pronoun, adj, noun);
+console.log(domainNames.join("\n"));
